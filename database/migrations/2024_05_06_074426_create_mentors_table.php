00586f241abfe1d6908a2d136fd91d12ae51cb0a
Frhsffiah <165320415+Frhsffiah@users.noreply.github.com>
2024-05-06 15:50:04 +0800

    u

---
+++ database/migrations/2024_05_06_074426_create_mentors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mentors', function (Blueprint $table) {
+            $table->string('Mentor_ID')->primary();
+            $table->string('MName');
+            $table->string('MNoPhone');
+            $table->string('MAddress');
+            $table->string('MGender');
+            $table->string('UserID')->references('UserID')->on('userprofiles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mentors');
+    }
+};
