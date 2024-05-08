1893126b1d7aabcb886035ea85650f87f3b521f3
Frhsffiah <165320415+Frhsffiah@users.noreply.github.com>
2024-05-08 15:27:44 +0800

    hi

---
+++ database/migrations/2024_05_08_071715_create_registrations_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->string('RegID')->primary();
+            $table->string('R_Type');
+            $table->string('R_Title');
+            $table->string('R_FullName');
+            $table->string('R_IC');
+            $table->string('R_Gender');
+            $table->string('R_Religion');
+            $table->string('R_Race');
+            $table->string('R_Citizenship');
+            $table->string('R_Address');
+            $table->string('R_PhoneNum');
+            $table->string('R_Email');
+            $table->string('R_FbName');
+            $table->string('R_CurrentEduLvl');
+            $table->string('R_EduField');
+            $table->string('R_EduInstitute');
+            $table->string('R_Occupation');
+            $table->string('R_Sponsorship');
+            $table->string('R_Program');
+            $table->string('R_Batch');
+            $table->string('Staff_ID')->references('Staff_ID')->on('staff');
+            $table->string('Platinum_ID')->references('Platinum_ID')->on('platinums');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registrations');
+    }
+};
