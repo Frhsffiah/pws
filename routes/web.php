ba76447e5388489853f5e30e2b55cf7d56931d08
Frhsffiah <165320415+Frhsffiah@users.noreply.github.com>
2024-05-13 14:26:10 +0800

    hi

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ForgotPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Login', [LoginController::class, 'LoginPage']);
+Route::get('/ForgotPassword', [ForgotPasswordController::class, 'ForgotPasswordPage']);
