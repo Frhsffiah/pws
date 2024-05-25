62e53fe545719e1be86b8c76cd0d5865919c3096
liyanakhairi <165324658+liyanakhairi@users.noreply.github.com>
2024-05-25 22:05:24 +0800

    auni nav

--- resources/views/expert/expertView.blade.php
+++ resources/views/expert/expertView.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ExpertView</title>
-</head>
-<body>
-    <h1>
-        Expert pAGAE
-    </h1>
-</body>
-</html>
+@extends('main.platinummain')
+
+@section('platinum')
+
+
+
+
+@endsection
